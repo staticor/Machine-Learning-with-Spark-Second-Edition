a4ed811d5ff1e634dddd1c8c2dfcde188745e957
staticor <staticor0828@gmail.com>
2017-09-26 11:39:02 +0800

    udpate

--- Chapter06/2.0.0/scala-spark-app/src/main/scala/org/sparksamples/classification/JinyDemo.scala
+++ Chapter06/2.0.0/scala-spark-app/src/main/scala/org/sparksamples/classification/JinyDemo.scala
@@ -1,9 +1,15 @@
 package org.sparksamples.classification
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 
 object JinyDemo {
 
   def main(args: Array[String]): Unit = {
 
+    val filepath = "/Users/steveyoung/Machine-Learning-with-Spark-Second-Edition/Chapter06/2.0.0/scala-spark-app/src/main/scala/org/sparksamples/classification/dataset"
+
+    val spConfig = new SparkConf().setMaster("local").setAppName("Lao nan hai")
+    val sc = new Sp
   }
 }
